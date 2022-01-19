ecce41fce7591965fa14e76e0df7eeb30fc4dcd3
jauharnotes <jauharnotes@gmail.com>
2022-01-19 21:17:52 +0700

    react meme generator

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,14 @@
 const Header = () => {
     return (
-        <div>
-            <div className="nav">
-                <div className="nav--logo">
-                    <img src="./troll-face.png" alt="logo" />
-                    <h2>Meme Generator</h2>
-                </div>
-                <div className="nav--project">
-                    <p>React Course - Project 3</p>
-                </div>
-            </div>
-        </div>
+        <header className="header">
+            <img
+                src="./troll-face.png"
+                className="header--image"
+                alt="logo"
+            />
+            <h2 className="header--title">Meme Generator</h2>
+            <h4 className="header--project">React Meme-generator</h4>
+        </header>
     )
 }
 
--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,36 +1,73 @@
-import { useState } from "react";
-import memesData from "../memeData";
+import React from "react"
+import memeData from "../memeData"
 
 export default function Meme() {
-    // const [memeImage, setMemeImage] = useState("")
-    const [meme, setMeme] = useState({
+    /**
+     * Challenge: 
+     * 1. Set up the text inputs to save to
+     *    the `topText` and `bottomText` state variables.
+     * 2. Replace the hard-coded text on the image with
+     *    the text being saved to state.
+     */
+
+    const [meme, setMeme] = React.useState({
         topText: "",
         bottomText: "",
         randomImage: "http://i.imgflip.com/1bij.jpg"
     })
 
-    const [allMeme, setAllMeme] = useState(memesData)
+    function handleChange(event) {
+        const { name, value, type } = event.target
+        setMeme(prevMeme => ({
+            ...prevMeme, [name]: value
+        }))
+    }
+
+    const [allMemeImages, setAllMemeImages] = React.useState(memeData)
+
 
     function getMemeImage() {
-        const data = allMeme.data.memes
-        const dataRandom = Math.floor(Math.random() * data.length)
-        const url = data[dataRandom].url
-        setMeme(revMemeImage => ({
-            ...revMemeImage,
+        const memesArray = allMemeImages.data.memes
+        const randomNumber = Math.floor(Math.random() * memesArray.length)
+        const url = memesArray[randomNumber].url
+        setMeme(prevMeme => ({
+            ...prevMeme,
             randomImage: url
         }))
+
     }
 
     return (
         <main>
-            <form className="form">
-                <input className="form--input" type="text" placeholder="Top tex" />
-                <input className="form--input" type="text" placeholder="Bottom Text" />
-                <button type="button" onClick={getMemeImage} className="form--button">
+            <div className="form">
+                <input
+                    type="text"
+                    placeholder="Top text"
+                    className="form--input"
+                    name="topText"
+                    onChange={handleChange}
+                    value={meme.topText}
+                />
+                <input
+                    type="text"
+                    placeholder="Bottom text"
+                    className="form--input"
+                    name="bottomText"
+                    onChange={handleChange}
+                    value={meme.bottomText}
+                />
+                <button
+                    className="form--button"
+                    onClick={getMemeImage}
+                >
                     Get a new meme image 🖼
                 </button>
-            </form>
-            <img className="meme--img" src={meme.randomImage} alt="meme" />
+            </div>
+            <div className="meme">
+                <img src={meme.randomImage} className="meme--image" alt="img" />
+                <h2 className="meme--text top">{meme.topText}</h2>
+                <h2 className="meme--text bottom">{meme.bottomText}</h2>
+            </div>
         </main>
-    );
-};
+    )
+}
